ed6709fef15b3f68535c5c2c7cea11fc5b9d25ba
TrevorNathan <tech25trevor@gmail.com>
2020-09-19 12:06:29 +0300

    UPDATE: configure mongoose connection to remote DB

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 
 const express = require("express");
 
+//import morgan: see paths in terminal:
 const morgan = require('morgan');
 
+//import dotenv: access db credentials securely:
 const dotenv = require('dotenv');
 //to use .env, need to invoke .config method:
 dotenv.config();
 
+//import mongoose: connect to mongodb:
+const mongoose = require('mongoose');
+
+
 //create express app:
 const app = express();
+
+
+//connect to DB with mongoose package:
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("SUCCESS! DB connected"));
+
+//create a callback:
+mongoose.connection.on("error", err => {
+    console.log('Ooh! FAILED Connection error: ${err.message}');
+});
 
 
 //export routes:
