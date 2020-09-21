b06a12d706af4a6be68b03855fa5bb4796c5b5b4
TrevorNathan <tech25trevor@gmail.com>
2020-09-21 10:45:02 +0300

    UPDATE: validate user input on post route

--- routes/post.js
+++ routes/post.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const postController = require('../controllers/post');
 
+//add validator: validate before going to controllers
+const validator = require("../validator");
+
+
 /**
  * OR:odirectly export methods:
  * const {getPosts, createPost} =  require('../controllers/post');
@@ -12,7 +16,8 @@ const router = express.Router();
 router.get("/", postController.getPosts);
 
 //create post route:
-router.post("/post", postController.createPost);
+//first validate before creating a post:
+router.post("/post", validator.createPostValidator, postController.createPost);
 
 module.exports = router;
 
